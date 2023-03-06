112e90345ba86cda0092ab3c1091ba268c0aefb8
Kai Jellinghaus <contact@kaij.tech>
2023-03-06 11:09:30 +0100

    Transpose Tests

--- benches/local_bench.rs
+++ benches/local_bench.rs
@@ -1,112 +1,12 @@
-use std::{future::IntoFuture, time::Duration};
+use std::time::Duration;
 
-use criterion::{
-    criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, BenchmarkId, Criterion,
-};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use futures::future::join;
-use queue_bench::{
-    local_queue::LocalQueue, postgres_queue::PostgresQueue, redis_queue::RedisQueue, Queue,
-};
+use queue_bench::{postgres_queue::PostgresQueue, redis_queue::RedisQueue, Queue};
 use rsmq_async::{PoolOptions, RsmqOptions};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let runtime = tokio::runtime::Runtime::new().unwrap();
-    fn run_configs_on_group<
-        T: 'static + Queue<ByteStruct, E> + Clone + Send + Sync,
-        E: std::fmt::Debug,
-        F1: Fn() -> Fut,
-        Fut: IntoFuture<Output = T>,
-    >(
-        runtime: &tokio::runtime::Runtime,
-        mut group: BenchmarkGroup<WallTime>,
-        connect: F1,
-    ) {
-        group.measurement_time(Duration::from_secs(60));
-        group.sample_size(60);
-
-        const KB: usize = 1024;
-        const CONFIGS: [i32; 1] = [100];
-        for size in [1 * KB, 4 * KB, 1024 * KB] {
-            for i in CONFIGS {
-                group.bench_with_input(
-                    BenchmarkId::new("enqueue", format!("{size} bytes x {i}")),
-                    &(i, size),
-                    |b, &(i, size)| {
-                        b.to_async(runtime).iter(|| async {
-                            let mut queue = connect().await;
-                            for _ in 0..i {
-                                queue.push(ByteStruct::new(size)).await.unwrap();
-                            }
-                        });
-                    },
-                );
-            }
-            for i in CONFIGS {
-                group.bench_with_input(
-                    BenchmarkId::new("push then pull", format!("{size} bytes x {i}")),
-                    &(i, size),
-                    |b, &(i, size)| {
-                        b.to_async(runtime).iter(|| async {
-                            let mut queue = connect().await;
-                            for _ in 0..i {
-                                queue.push(ByteStruct::new(size)).await.unwrap();
-                            }
-                            for _ in 0..i {
-                                queue.try_pull().await.unwrap().unwrap();
-                            }
-                        });
-                    },
-                );
-            }
-            for i in CONFIGS {
-                group.bench_with_input(
-                    BenchmarkId::new("push mix pull", format!("{size} bytes x {i}")),
-                    &(i, size),
-                    |b, &(i, size)| {
-                        b.to_async(runtime).iter(|| async {
-                            let mut queue = connect().await;
-                            for _ in 0..i {
-                                queue.push(ByteStruct::new(size)).await.unwrap();
-                                queue.try_pull().await.unwrap().unwrap();
-                            }
-                        });
-                    },
-                );
-            }
-            for i in CONFIGS {
-                group.bench_with_input(
-                    BenchmarkId::new("push & pull concurrent", format!("{size} bytes x {i}")),
-                    &(i, size),
-                    |b, &(i, size)| {
-                        b.to_async(runtime).iter(|| async {
-                            let mut queue = connect().await;
-
-                            let q = queue.clone();
-                            let pusher = tokio::spawn(async move {
-                                let mut queue = q;
-                                for _ in 0..i {
-                                    queue.push(ByteStruct::new(size)).await.unwrap();
-                                }
-                            });
-                            let q = queue.clone();
-                            let puller = tokio::spawn(async move {
-                                let mut queue = q;
-                                for _ in 0..i {
-                                    while let None = queue.try_pull().await.unwrap() {}
-                                }
-                            });
-
-                            let (a, b) = join(pusher, puller).await;
-                            a.unwrap();
-                            b.unwrap();
-                        });
-                    },
-                );
-            }
-        }
-
-        group.finish();
-    }
+    let runtime = &tokio::runtime::Runtime::new().unwrap();
 
     let postgres_url = "postgresql://postgres:changeme@localhost:5432/test-db";
 
@@ -130,41 +30,241 @@ fn criterion_benchmark(c: &mut Criterion) {
         ))
         .unwrap();
 
-    run_configs_on_group::<RedisQueue, _, _, _>(
-        &runtime,
-        c.benchmark_group("redis queue"),
-        || async {
-            RedisQueue::new(
-                rsmq.clone(),
-                uuid::Builder::from_random_bytes(rand::random())
-                    .as_uuid()
-                    .to_string(),
-            )
-            .await
-            .unwrap()
-        },
-    );
+    const KB: usize = 1024;
+    const CONFIGS: [i32; 1] = [500];
+    for size in [1 * KB, 4 * KB, 1024 * KB] {
+        for i in CONFIGS {
+            let mut group = c.benchmark_group(format!("enqueue {size} bytes x {i}"));
 
-    run_configs_on_group::<PostgresQueue, _, _, _>(
-        &runtime,
-        c.benchmark_group("postgres queue"),
-        || async {
-            PostgresQueue::new(
-                pql.clone(),
-                uuid::Builder::from_random_bytes(rand::random())
-                    .as_uuid()
-                    .to_string(),
-            )
-            .await
-            .unwrap()
-        },
-    );
+            group.measurement_time(Duration::from_secs(60));
+            group.sample_size(60);
 
-    run_configs_on_group::<LocalQueue<_>, _, _, _>(
-        &runtime,
-        c.benchmark_group("local queue"),
-        || async { LocalQueue::new().unwrap() },
-    );
+            group.bench_with_input(
+                BenchmarkId::new("redis", ""),
+                &(i, size),
+                |b, &(i, size)| {
+                    b.to_async(runtime).iter(|| async {
+                        let mut queue = RedisQueue::new(
+                            rsmq.clone(),
+                            uuid::Builder::from_random_bytes(rand::random())
+                                .as_uuid()
+                                .to_string(),
+                        )
+                        .await
+                        .unwrap();
+                        for _ in 0..i {
+                            queue.push(ByteStruct::new(size)).await.unwrap();
+                        }
+                    });
+                },
+            );
+            group.bench_with_input(
+                BenchmarkId::new("postgres", ""),
+                &(i, size),
+                |b, &(i, size)| {
+                    b.to_async(runtime).iter(|| async {
+                        let mut queue = PostgresQueue::new(
+                            pql.clone(),
+                            uuid::Builder::from_random_bytes(rand::random())
+                                .as_uuid()
+                                .to_string(),
+                        )
+                        .await
+                        .unwrap();
+                        for _ in 0..i {
+                            queue.push(ByteStruct::new(size)).await.unwrap();
+                        }
+                    });
+                },
+            );
+
+            group.finish();
+
+            let mut group = c.benchmark_group(format!("push then pull {size} bytes x {i}"));
+
+            group.measurement_time(Duration::from_secs(60));
+            group.sample_size(60);
+            group.bench_with_input(
+                BenchmarkId::new("redis", ""),
+                &(i, size),
+                |b, &(i, size)| {
+                    b.to_async(runtime).iter(|| async {
+                        let mut queue = RedisQueue::new(
+                            rsmq.clone(),
+                            uuid::Builder::from_random_bytes(rand::random())
+                                .as_uuid()
+                                .to_string(),
+                        )
+                        .await
+                        .unwrap();
+                        for _ in 0..i {
+                            queue.push(ByteStruct::new(size)).await.unwrap();
+                        }
+                        for _ in 0..i {
+                            (queue.try_pull().await.unwrap() as Option<ByteStruct>).unwrap();
+                        }
+                    });
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("postgres", ""),
+                &(i, size),
+                |b, &(i, size)| {
+                    b.to_async(runtime).iter(|| async {
+                        let mut queue = PostgresQueue::new(
+                            pql.clone(),
+                            uuid::Builder::from_random_bytes(rand::random())
+                                .as_uuid()
+                                .to_string(),
+                        )
+                        .await
+                        .unwrap();
+                        for _ in 0..i {
+                            queue.push(ByteStruct::new(size)).await.unwrap();
+                        }
+                        for _ in 0..i {
+                            (queue.try_pull().await.unwrap() as Option<ByteStruct>).unwrap();
+                        }
+                    });
+                },
+            );
+
+            group.finish();
+
+            let mut group = c.benchmark_group(format!("push mix pull {size} bytes x {i}"));
+
+            group.measurement_time(Duration::from_secs(60));
+            group.sample_size(60);
+            group.bench_with_input(
+                BenchmarkId::new("redis", ""),
+                &(i, size),
+                |b, &(i, size)| {
+                    b.to_async(runtime).iter(|| async {
+                        let mut queue = RedisQueue::new(
+                            rsmq.clone(),
+                            uuid::Builder::from_random_bytes(rand::random())
+                                .as_uuid()
+                                .to_string(),
+                        )
+                        .await
+                        .unwrap();
+                        for _ in 0..i {
+                            queue.push(ByteStruct::new(size)).await.unwrap();
+                            (queue.try_pull().await.unwrap() as Option<ByteStruct>).unwrap();
+                        }
+                    });
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("postgres", ""),
+                &(i, size),
+                |b, &(i, size)| {
+                    b.to_async(runtime).iter(|| async {
+                        let mut queue = PostgresQueue::new(
+                            pql.clone(),
+                            uuid::Builder::from_random_bytes(rand::random())
+                                .as_uuid()
+                                .to_string(),
+                        )
+                        .await
+                        .unwrap();
+                        for _ in 0..i {
+                            queue.push(ByteStruct::new(size)).await.unwrap();
+                            (queue.try_pull().await.unwrap() as Option<ByteStruct>).unwrap();
+                        }
+                    });
+                },
+            );
+
+            group.finish();
+
+            let mut group = c.benchmark_group(format!("push & pull concurrent {size} bytes x {i}"));
+
+            group.measurement_time(Duration::from_secs(60));
+            group.sample_size(60);
+            group.bench_with_input(
+                BenchmarkId::new("redis", ""),
+                &(i, size),
+                |b, &(i, size)| {
+                    b.to_async(runtime).iter(|| async {
+                        let queue = RedisQueue::new(
+                            rsmq.clone(),
+                            uuid::Builder::from_random_bytes(rand::random())
+                                .as_uuid()
+                                .to_string(),
+                        )
+                        .await
+                        .unwrap();
+
+                        let q = queue.clone();
+                        let pusher = tokio::spawn(async move {
+                            let mut queue = q;
+                            for _ in 0..i {
+                                queue.push(ByteStruct::new(size)).await.unwrap();
+                            }
+                        });
+                        let q = queue.clone();
+                        let puller = tokio::spawn(async move {
+                            let mut queue = q;
+                            for _ in 0..i {
+                                while let None =
+                                    queue.try_pull().await.unwrap() as Option<ByteStruct>
+                                {
+                                }
+                            }
+                        });
+
+                        let (a, b) = join(pusher, puller).await;
+                        a.unwrap();
+                        b.unwrap();
+                    });
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new("postgres", ""),
+                &(i, size),
+                |b, &(i, size)| {
+                    b.to_async(runtime).iter(|| async {
+                        let queue = PostgresQueue::new(
+                            pql.clone(),
+                            uuid::Builder::from_random_bytes(rand::random())
+                                .as_uuid()
+                                .to_string(),
+                        )
+                        .await
+                        .unwrap();
+
+                        let q = queue.clone();
+                        let pusher = tokio::spawn(async move {
+                            let mut queue = q;
+                            for _ in 0..i {
+                                queue.push(ByteStruct::new(size)).await.unwrap();
+                            }
+                        });
+                        let q = queue.clone();
+                        let puller = tokio::spawn(async move {
+                            let mut queue = q;
+                            for _ in 0..i {
+                                while let None =
+                                    queue.try_pull().await.unwrap() as Option<ByteStruct>
+                                {
+                                }
+                            }
+                        });
+
+                        let (a, b) = join(pusher, puller).await;
+                        a.unwrap();
+                        b.unwrap();
+                    });
+                },
+            );
+
+            group.finish();
+        }
+    }
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Clone)]
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,7 @@ pub mod postgres_queue;
 pub mod redis_queue;
 
 #[async_trait::async_trait]
-pub trait Queue<T, E>
-where
-    Self: Sized,
-{
+pub trait Queue<T, E> {
     async fn try_pull(&mut self) -> Result<Option<T>, E>;
     async fn push(&mut self, value: T) -> Result<(), E>;
 }
